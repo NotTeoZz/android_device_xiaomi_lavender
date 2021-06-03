6ef232109604cdd66c1ce629f5a5e352330e18cb
NotTeoZz <matteomeggiolaro04@gmail.com>
2021-06-03 17:56:33 +0200

    Import some changes for Pixel Extended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/pex_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    pex_lavender-user \
+    pex_lavender-userdebug \
+    pex_lavender-eng
